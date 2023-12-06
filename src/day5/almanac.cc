929b7be022d1adeeb4f3f3e1b30aaf8b406d69a2
Joni Heinonen <joni.heinonen@gmail.com>
2023-12-06 12:04:15 +0200

    Type simplification
    Added some clarifying comment
    Renamed variables to make their intent clearer

--- src/day5/almanac.cc
+++ src/day5/almanac.cc
@@ -2,8 +2,8 @@
 #include <iostream>
 #include "almanac.hh"
 
-std::vector<uint64_t> read_numbers_from_row(const std::string& row, char splitter = ' ') {
-    auto result = std::vector<uint64_t>();
+std::vector<int64_t> read_numbers_from_row(const std::string& row, char splitter = ' ') {
+    auto result = std::vector<int64_t>();
     size_t start_it = 0;
     auto end_it = row.find_first_of(splitter);
     while(end_it != std::string::npos) {
@@ -49,10 +49,10 @@ void Almanac::read_input(const std::vector<std::string>& input)
     }
 }
 
-uint64_t Almanac::find_smallest_location(uint64_t start, uint64_t range)
+int64_t Almanac::find_smallest_location(int64_t start, int64_t range)
 {
     std::string end = "location";
-    uint64_t min_value = std::numeric_limits<uint64_t>::max();
+    int64_t min_value = std::numeric_limits<int64_t>::max();
     for (auto seed = start; seed < start + range; ++seed) {
         std::string current_source = "seed";
         auto current_value = seed;
@@ -75,62 +75,69 @@ uint64_t Almanac::find_smallest_location(uint64_t start, uint64_t range)
     return min_value;
 }
 
-uint64_t Almanac::find_smallest_location_indv_seeds()
+int64_t Almanac::find_smallest_location_indv_seeds()
 {
-    auto min_value = std::numeric_limits<uint64_t>::max();
+    auto min_value = std::numeric_limits<int64_t>::max();
     for(const auto& seed : _seeds) {
         min_value = std::min(find_smallest_loc_ranges(min_value, seed, 0, "seed"), min_value);
     }
     return min_value;
 }
 
-uint64_t Almanac::find_smallest_location_ranges()
+int64_t Almanac::find_smallest_location_ranges()
 {
-    auto values = std::vector<uint64_t>();
+    auto values = std::vector<int64_t>();
     for(size_t i = 0; i < _seeds.size(); i += 2) {
-        std::cout << "Starting range: " << _seeds[i] << " spanning " << _seeds[i + 1] << " seeds\n";
-        values.push_back(find_smallest_loc_ranges(std::numeric_limits<uint64_t>::max(), _seeds[i], _seeds[i+1], "seed"));
-        std::cout << "Previous min value: " << values.back() << "\n";
+        values.push_back(find_smallest_loc_ranges(std::numeric_limits<int64_t>::max(), _seeds[i], _seeds[i+1], "seed"));
     }
     return *std::min_element(values.begin(), values.end());
 }
 
-uint64_t Almanac::find_smallest_loc_ranges(uint64_t curr_min, uint64_t range_start, uint64_t length, std::string source) {
+int64_t Almanac::find_smallest_loc_ranges(int64_t curr_min, int64_t range_start, int64_t length, std::string source) {
     if(source == "location") {
         curr_min = std::min(range_start, curr_min);
         return curr_min;
     }
     std::vector<std::shared_ptr<SimpleRange>> ranges = {std::make_shared<SimpleRange>(range_start, length)};
     auto range_mapping = &_by_source_mapping.at(source);
-    while(ranges.size() > 0) {
+    while(!ranges.empty()) {
         auto curr_range = ranges.back();
         ranges.pop_back();
         auto target_ranges = range_mapping->ranges;
         bool found_range = false;
         for(size_t i = 0; i < target_ranges.size(); ++i) {
-            auto part_range = curr_range->part_in_range(*target_ranges[i]);
-            if (part_range != SimpleRange::WRONG_PAIR) {
+            auto shared_range = curr_range->part_in_range(*target_ranges[i]);
+            if (shared_range != SimpleRange::NOT_FOUND) {
                 found_range = true;
-                if (part_range.first != curr_range->start) {
-                    ranges.push_back(std::make_shared<SimpleRange>(curr_range->start, part_range.first - curr_range->start - 1));
+                if (shared_range.first != curr_range->start) {
+                    // The left edge has of new range has to be at start of the current range.
+                    // The right edge is one before the start of the shared range,
+                    // thus cutting of one from the length makes new range correct
+                    ranges.push_back(std::make_shared<SimpleRange>(curr_range->start,
+                                                                   shared_range.first - curr_range->start - 1));
 
                 }
-                if (part_range.second != curr_range->end) {
-                    ranges.push_back(std::make_shared<SimpleRange>(part_range.second + 1, curr_range->end - part_range.second - 1));
+                if (shared_range.second != curr_range->end) {
+                    // Here the left edge is one after the shared range end and
+                    // right edge is the right edge of the current range. Thus left has to
+                    // start one after shared range and length has to be one shorter as well
+                    // to not go over right edge.
+                    ranges.push_back(std::make_shared<SimpleRange>(shared_range.second + 1,
+                                                                   curr_range->end - shared_range.second - 1));
                 }
-                auto translated_range_start = target_ranges[i]->translate(part_range.first);
-                curr_min = std::min(find_smallest_loc_ranges(curr_min, translated_range_start, part_range.second - part_range.first,
-                                                                _by_source_mapping.at(source).destination),
-                                    curr_min);
-                if (curr_range->start == part_range.first && curr_range->end == part_range.second) {
+                auto translated_range_start = target_ranges[i]->translate(shared_range.first);
+                curr_min = std::min(find_smallest_loc_ranges(curr_min, translated_range_start,
+                                                             shared_range.second - shared_range.first,
+                                                             range_mapping->destination), curr_min);
+                if (curr_range->start == shared_range.first && curr_range->end == shared_range.second) {
                     break;
                 }
             }
         }
         if (!found_range) {
-            curr_min = std::min(find_smallest_loc_ranges(curr_min, curr_range->start, curr_range->end - curr_range->start,
-                                                            _by_source_mapping.at(source).destination),
-                                curr_min);
+            curr_min = std::min(find_smallest_loc_ranges(curr_min, curr_range->start,
+                                                         curr_range->end - curr_range->start,
+                                                         range_mapping->destination), curr_min);
         }
     }
     return curr_min;
--- src/day5/almanac.hh
+++ src/day5/almanac.hh
@@ -10,53 +10,42 @@
 #include <inttypes.h>
 
 struct Range {
-    uint64_t start;
-    uint64_t end;
-    uint64_t difference;
-    bool direction;
+    int64_t start;
+    int64_t end;
+    int64_t difference;
 
-    bool inline is_in_range(uint64_t num) {
+    bool inline is_in_range(int64_t num) {
         return num >= start && num <= end;
     }
 
-    uint64_t inline translate(uint64_t to_translate) {
-        if(direction) {
-            return to_translate + difference;
-        }
-        return to_translate - difference;
+    int64_t inline translate(int64_t to_translate) {
+        return to_translate + difference;
     }
 
-    Range(uint64_t d_start, uint64_t s_start, uint64_t length) {
+    Range(int64_t d_start, int64_t s_start, int64_t length) {
         start = s_start;
         end = start + length - 1; // 0 indexed
-        if (d_start > s_start) {
-            difference = d_start - s_start;
-            direction = true;
-        }
-        else {
-            difference = s_start - d_start;
-            direction = false;
-        }
+        difference = d_start - s_start;
     }
 };
 
 struct SimpleRange {
-    uint64_t start;
-    uint64_t end;
+    int64_t start;
+    int64_t end;
 
-    static inline std::pair<uint64_t, uint64_t> WRONG_PAIR =
-      std::pair<uint64_t, uint64_t> {std::numeric_limits<uint64_t>::max(), std::numeric_limits<uint64_t>::max()};
+    static inline std::pair<int64_t, int64_t> NOT_FOUND =
+      std::pair<int64_t, int64_t> {std::numeric_limits<int64_t>::max(), std::numeric_limits<int64_t>::max()};
 
-    std::pair<uint64_t, uint64_t> part_in_range(Range t_range) {
-        uint64_t min_val = std::max(start, t_range.start);
-        uint64_t max_val = std::min(end, t_range.end);
+    std::pair<int64_t, int64_t> part_in_range(Range t_range) {
+        int64_t min_val = std::max(start, t_range.start);
+        int64_t max_val = std::min(end, t_range.end);
         if (min_val <= max_val) {
             return {min_val, max_val};
         }
-        return WRONG_PAIR;
+        return NOT_FOUND;
     }
 
-    SimpleRange(uint64_t start_, uint64_t length) {
+    SimpleRange(int64_t start_, int64_t length) {
         start = start_;
         end = start + length;
     }
@@ -82,16 +71,16 @@ public:
     ~Almanac() = default;
 
     void read_input(const std::vector<std::string>& input);
-    uint64_t find_smallest_location(uint64_t start, uint64_t range);
-    uint64_t find_smallest_location_indv_seeds();
-    uint64_t find_smallest_location_ranges();
+    int64_t find_smallest_location(int64_t start, int64_t range);
+    int64_t find_smallest_location_indv_seeds();
+    int64_t find_smallest_location_ranges();
 
 private:
-    std::vector<uint64_t> _seeds = {};
+    std::vector<int64_t> _seeds = {};
     std::unordered_map<std::string, SourceDestMapping> _by_source_mapping = {};
 
-    uint64_t find_smallest_loc_ranges(uint64_t curr_min, uint64_t range_start,
-                                      uint64_t length, std::string source);
+    int64_t find_smallest_loc_ranges(int64_t curr_min, int64_t range_start,
+                                      int64_t length, std::string source);
 };
 
 #endif // ALMANAC_HH
